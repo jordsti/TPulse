54c9067edd585a0e0f83677e157ae663e16dcc63
= <jord52@gmail.com>
2013-05-28 01:08:26 -0400

    abstract manager 	new file:   TPulseAPI/DB/BaseManager.cs 	modified:   TPulseAPI/TPulseAPI.csproj

---
+++ TPulseAPI/DB/BaseManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Text;
+using MySql.Data.MySqlClient;
+using TPulseAPI.Extensions;
+
+namespace TPulseAPI.DB
+{
+    public abstract class BaseManager
+    {
+        protected IDbConnection Database;
+        public String TableName { get; protected set; }
+
+        protected BaseManager(IDbConnection database, string tableName)
+        {
+            Database = database;
+            TableName = tableName;
+        }
+
+        protected IQueryBuilder QueryBuilder
+        {
+            get
+            {
+                if (Database.GetSqlType() == SqlType.Sqlite)
+                    return new SqliteQueryCreator();
+                else
+                    return new MysqlQueryCreator();
+            }
+        }
+
+
+    }
+}
